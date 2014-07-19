6842473f954735a1dc44025eb715e2d287073811
Kirn Gill II <segin2005@gmail.com>
2014-07-19 10:16:07 -0500

    AndroidManifest.xml: Hooked title for SettingsActivity into existing "Settings" string TTLEditor.java: Enabled "Settings" menu option res/menu/ttleditor.xml: Enabled "Settings" menu option

--- app/src/main/java/org/segin/ttleditor/TTLEditor.java
+++ app/src/main/java/org/segin/ttleditor/TTLEditor.java
@@ -284,11 +284,11 @@ public class TTLEditor extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        /* if (id == R.id.action_settings) {
+        if (id == R.id.action_settings) {
             Intent i = new Intent(TTLEditor.this, SettingsActivity.class);
             TTLEditor.this.startActivity(i);
             return true;
-        } */
+        }
         if (id == R.id.action_about) {
             buildAboutDialog();
         }
