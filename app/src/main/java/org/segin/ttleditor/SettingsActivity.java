b038857037b4e8f94216a1286fd37e4aa5e1d511
Kirn Gill II <segin2005@gmail.com>
2014-07-19 20:04:52 -0500

    1.0.4 snapshot

--- app/src/main/java/org/segin/ttleditor/SettingsActivity.java
+++ app/src/main/java/org/segin/ttleditor/SettingsActivity.java
@@ -2,6 +2,7 @@ package org.segin.ttleditor;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
@@ -60,18 +61,16 @@ public class SettingsActivity extends PreferenceActivity {
     }
 
     @Override
+    protected boolean isValidFragment (String fragmentName) {
+        if(SettingsActivity.class.getName().equals(fragmentName))
+            return true;
+        return false;
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == android.R.id.home) {
-            // This ID represents the Home or Up button. In the case of this
-            // activity, the Up button is shown. Use NavUtils to allow users
-            // to navigate up one level in the application structure. For
-            // more details, see the Navigation pattern on Android Design:
-            //
-            // http://developer.android.com/design/patterns/navigation.html#up-vs-back
-            //
-            //
-            // that hierarchy.
             NavUtils.navigateUpFromSameTask(this);
             return true;
         }
@@ -113,6 +112,19 @@ public class SettingsActivity extends PreferenceActivity {
         // to reflect the new value, per the Android Design guidelines.
         bindPreferenceSummaryToValue(findPreference("iface"));
         bindPreferenceSummaryToValue(findPreference("ttl"));
+        Preference sharePref = (Preference) findPreference("share");
+        sharePref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            public boolean onPreferenceClick(Preference preference) {
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_text));
+                sendIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.share_subject));
+                sendIntent.setType("text/plain");
+                startActivity(sendIntent);
+                return true;
+            }
+        });
+
     }
 
     /** {@inheritDoc} */
@@ -199,7 +211,21 @@ public class SettingsActivity extends PreferenceActivity {
 
             bindPreferenceSummaryToValue(findPreference("iface"));
             bindPreferenceSummaryToValue(findPreference("ttl"));
+            Preference sharePref = (Preference) findPreference("share");
+            sharePref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                public boolean onPreferenceClick(Preference preference) {
+                    Intent sendIntent = new Intent();
+                    sendIntent.setAction(Intent.ACTION_SEND);
+                    sendIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_text));
+                    sendIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.share_subject));
+                    sendIntent.setType("text/plain");
+                    startActivity(sendIntent);
+                    return true;
+                }
+            });
+
         }
+
     }
 
     /**
