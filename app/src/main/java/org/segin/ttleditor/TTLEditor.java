bd0e8dccc854cc2ab89b094590b3b3a10bd277c5
Kirn Gill II <segin2005@gmail.com>
2014-07-27 23:39:36 -0500

    Added license headers

--- app/src/main/java/org/segin/ttleditor/TTLEditor.java
+++ app/src/main/java/org/segin/ttleditor/TTLEditor.java
@@ -1,5 +1,21 @@
 package org.segin.ttleditor;
 
+/*
+ * Copyright 2014 Kirn Gill II <segin2005@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
@@ -63,7 +79,7 @@ public class TTLEditor extends Activity {
                     .setNegativeButton(getString(android.R.string.ok), new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int id) {
                             try {
-                                TTLEditor.this.finalize();
+
                             } catch (Throwable e) {
                                 Log.wtf("TTLEditor", "wtf, cannot Activity.finalize()?", e);
                             }
